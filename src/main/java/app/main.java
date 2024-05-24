4376543a143f3b8e442197d82578d6c35fd0e128
61610 <61610@etu.he2b.be>
2024-05-24 13:21:42 +0200

    PomXml fixed javafx

--- src/main/java/app/main.java
+++ src/main/java/app/main.java
@@ -6,8 +6,6 @@ import mvp.presentation.Presentation;
 import mvp.view.InputView;
 import mvp.view.MainView;
 
-// --module-path "\path\to\javafx-sdk-17\lib" --add-modules javafx.controls,javafx.fxml
-
 public class main extends Application {
     public static void main(String[] args) throws Exception {
         Application.launch(args);
